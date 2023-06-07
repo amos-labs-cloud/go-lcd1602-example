2a83f4f945b132d7fb88b4efeb052bc55102a1c7
edwinavalos <edwin.a.avalos@gmail.com>
2023-06-06 19:22:47 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package go_lcd1602_example
+
+import (
+	"github.com/pimvanhespen/go-pi-lcd1602"
+	"github.com/pimvanhespen/go-pi-lcd1602/synchronized"
+)
+
+func main() {
+	lcdi := lcd1602.New(18, 23, []int{24, 25, 12, 16}, 16)
+	syncedLCD := synchronized.NewSynchronizedLCD(lcdi)
+	syncedLCD.Initialize()
+	defer syncedLCD.Close()
+
+	syncedLCD.WriteLines("Hello world!")
+}
